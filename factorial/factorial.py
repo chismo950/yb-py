3def60271d9b1a2fbdf27dd235fa253dd54d9814
Eason <craigchismo950@gmail.com>
2025-04-26 09:21:36 +1200

    deal with negative number

--- factorial/factorial.py
+++ factorial/factorial.py
@@ -1,4 +1,7 @@
 input_n = int(input("Enter a number: "))
+if input_n < 0:
+    print("Factorial is not defined for negative numbers")
+    exit(1)
 def factorial(n: int) -> int:
     if n == 0:
         return 1
