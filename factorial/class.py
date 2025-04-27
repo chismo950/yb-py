39924811ef25e243d37b9869b680b7425064bbaf
Eason <craigchismo950@gmail.com>
2025-04-27 12:00:25 +1200

    factorial class

---
+++ factorial/class.py
@@ -0,0 +1,21 @@
+class Factorial:
+    def input(self):
+        self.n = int(input("Enter a positive integer: "))
+        if self.n < 0:
+            raise ValueError("Factorial is not defined for negative numbers.")
+        return self.n
+
+    def calculate(self):
+        if self.n < 0:
+            raise ValueError("Factorial is not defined for negative numbers.")
+        elif self.n == 0 or self.n == 1:
+            return 1
+        else:
+            result = 1
+            for i in range(2, self.n + 1):
+                result *= i
+            return result
+
+factorial = Factorial()
+factorial.input()
+print(f"factorial of {factorial.n} is", factorial.calculate())
