287a67efb6b8af015669bbb97b79596fca1d2883
Eason <craigchismo950@gmail.com>
2025-05-04 11:26:16 +1200

    Activity W4-2-1

---
+++ Activity-W4-2-1/main.py
@@ -0,0 +1,19 @@
+import os
+import pandas as pd
+
+baseDir = os.path.dirname(os.path.abspath(__file__))
+
+# 1- load the text file and find the number of __ in the txt file
+txtPath = baseDir + "/sample_text.txt"
+txtFile = open(txtPath)
+txtContent = txtFile.read()
+numOfDoubleUnderScore = txtContent.count("__")
+print('numOfDoubleUnderScore: ', numOfDoubleUnderScore)
+
+# 2- load the parquet file and print out the number of features
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
+parquetPath = baseDir + "/Sample_data_2.parquet"
+df = pd.read_parquet(parquetPath)
+num_features = len(df.columns)
+print(num_features)
