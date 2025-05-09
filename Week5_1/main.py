29c8b0a794273cf69e970ae0847d595f25894624
Eason <craigchismo950@gmail.com>
2025-05-10 09:06:21 +1200

    sqlite

---
+++ Week5_1/main.py
@@ -0,0 +1,53 @@
+from database import create_table
+from user_manager import add_user, view_users, search_user, delete_user, update_user, search_user_advanced
+
+def menu():
+    print("\n==== User Manager ====")
+    print("1. Add User")
+    print("2. View All Users")
+    print("3. Search User by Name")
+    print("4. Delete User by ID")
+    print("5. Exit")
+    print("6. Search By ID and Name")
+    print("7. Update User by ID")
+
+def main():
+    create_table()
+    while True:
+        menu()
+        choice = input("Select an option (1-5): ")
+        if choice == '1':
+            name = input("Enter name: ")
+            email = input("Enter email: ")
+            add_user(name, email)
+        elif choice == '2':
+            users = view_users()
+            for user in users:
+                print(user)
+        elif choice == '3':
+            name = input("Enter name to search: ")
+            users = search_user(name)
+            for user in users:
+                print(user)
+        elif choice == '4':
+            user_id = int(input("Enter user ID to delete: "))
+            delete_user(user_id)
+        elif choice == '5':
+            print("Goodbye!")
+            break
+        if choice == '7':
+            user_id = input("Enter User ID: ")
+            name = input("Enter name: ")
+            email = input("Enter email: ")
+            update_user(user_id, name, email)
+        elif choice == '6':
+            user_id = input("Enter User ID: ")
+            name = input("Enter name: ")
+            user = search_user_advanced(user_id, name)
+            print(user)
+                
+        else:
+            print("Invalid choice, try again.")
+
+if __name__ == "__main__":
+    main()
---
+++ Week5_1/user_manager.py
@@ -0,0 +1,56 @@
+from database import create_connection
+import sqlite3
+
+def add_user(name, email):
+    conn = create_connection()
+    cursor = conn.cursor()
+    try:
+        cursor.execute("INSERT INTO users (name, email) VALUES (?, ?)", (name, email))
+        conn.commit()
+        print(" User added successfully.")
+    except sqlite3.IntegrityError:
+        print(" Email must be unique.")
+    conn.close()
+
+def view_users():
+    conn = create_connection()
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM users")
+    rows = cursor.fetchall()
+    conn.close()
+    return rows
+
+def search_user(name):
+    conn = create_connection()
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM users WHERE name LIKE ?", ('%' + name + '%',))
+    rows = cursor.fetchall()
+    conn.close()
+    return rows
+
+def delete_user(user_id):
+    conn = create_connection()
+    cursor = conn.cursor()
+    cursor.execute("DELETE FROM users WHERE id = ?", (user_id,))
+    conn.commit()
+    conn.close()
+    print("🗑️ User deleted.")
+
+def update_user(user_id:int, name:str, email:str):
+    conn = create_connection()
+    cursor = conn.cursor()
+    cursor.execute(
+        "UPDATE users SET name = ?, email = ? WHERE id = ?", 
+        (user_id, name, email)
+    )
+    conn.commit()
+    conn.close()
+    print("✅ User updated.")
+
+def search_user_advanced(user_id: int, name: str):
+    conn = create_connection()
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM users WHERE name LIKE ? and id = ?", ('%' + name + '%', user_id))
+    rows = cursor.fetchone()
+    conn.close()
+    return rows
